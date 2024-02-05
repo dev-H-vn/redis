bdcc1e4264c523706a50de963e55a10c40c62df7
huyld <huy.ld@orai.io>
2024-02-05 12:41:25 +0700

    first

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApiConfigService } from 'src/share/services/api-config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      // imports: [SharedModule],
+      useFactory: (configService: ApiConfigService) =>
+        configService.mySqlConfig,
+      inject: [ApiConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
